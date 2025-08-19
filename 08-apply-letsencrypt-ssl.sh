5ca2e75ead5d2bd9b61cf876696bf87cb1491aaa
Deepen Dhulla <deepen@deependhulla.com>
2025-08-19 19:00:13 +0530

    update on Tue Aug 19 07:00:13 PM IST 2025

--- 08-apply-letsencrypt-ssl.sh
+++ 08-apply-letsencrypt-ssl.sh
@@ -12,14 +12,12 @@ sed -i "s/#RewriteEngine/RewriteEngine/" /etc/apache2/sites-available/000-defaul
 sed -i "s/#RewriteCond/RewriteCond/" /etc/apache2/sites-available/000-default.conf
 sed -i "s/#RewriteRule/RewriteRule/" /etc/apache2/sites-available/000-default.conf
 ln -vs /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/
-/etc/init.d/apache2 restart
 
 sed -i "s/ssl_key = <\/etc\/ssl\/private\/ssl-cert-snakeoil\.key//" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/ssl_cert = <\/etc\/ssl\/certs\/ssl-cert-snakeoil\.pem//" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/#xssl_cert/ssl_cert/" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/#xssl_key/ssl_key/" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /etc/dovecot/conf.d/10-ssl.conf
-/etc/init.d/dovecot restart
 
 sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /etc/postfix/main.cf
 sed -i "s/smtpd_tls_cert_file=\/etc\/ssl\/certs\/ssl-cert-snakeoil\.pem//" /etc/postfix/main.cf
@@ -31,12 +29,26 @@ sed -i "s/#xsmtpd_tls_cert_file/smtpd_tls_cert_file/" /etc/postfix/main.cf
 sed -i "s/#xsmtpd_tls_key_file/smtpd_tls_key_file/" /etc/postfix/main.cf
 sed -i "s/#xsmtp_tls_cert_file/smtp_tls_cert_file/" /etc/postfix/main.cf
 sed -i "s/#xsmtp_tls_key_file/smtp_tls_key_file/" /etc/postfix/main.cf
-/etc/init.d/postfix restart
-systemctl restart postfix-inbound
+
+sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /etc/postfix-inbound/main.cf
+sed -i "s/smtpd_tls_cert_file=\/etc\/ssl\/certs\/ssl-cert-snakeoil\.pem//" /etc/postfix-inbound/main.cf
+sed -i "s/smtpd_tls_key_file=\/etc\/ssl\/private\/ssl-cert-snakeoil\.key//" /etc/postfix-inbound/main.cf
+sed -i "s/smtp_tls_cert_file=\/etc\/ssl\/certs\/ssl-cert-snakeoil\.pem//" /etc/postfix-inbound/main.cf
+sed -i "s/smtp_tls_key_file=\/etc\/ssl\/private\/ssl-cert-snakeoil\.key//" /etc/postfix-inbound/main.cf
+
+sed -i "s/#xsmtpd_tls_cert_file/smtpd_tls_cert_file/" /etc/postfix-inbound/main.cf
+sed -i "s/#xsmtpd_tls_key_file/smtpd_tls_key_file/" /etc/postfix-inbound/main.cf
+sed -i "s/#xsmtp_tls_cert_file/smtp_tls_cert_file/" /etc/postfix-inbound/main.cf
+sed -i "s/#xsmtp_tls_key_file/smtp_tls_key_file/" /etc/postfix-inbound/main.cf
+
 cat /etc/letsencrypt/live/`hostname -f`/fullchain.pem > /etc/webmin/miniserv.pem
 cat /etc/letsencrypt/live/`hostname -f`/privkey.pem >> /etc/webmin/miniserv.pem
+
+systemctl restart apache2
+systemctl restart dovecot
+systemctl restart postfix
+systemctl restart postfix-inbound
 systemctl restart webmin
 
-#cp files/rootdir/usr/local/src/cert-renew-and-restart.sh /usr/local/src/
 sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /usr/local/src/cert-renew-and-restart.sh
 
