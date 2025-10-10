d24deb0d997597cd76fbaf41b1eeed4586f6d08e
Deepen Dhulla <deepen@deependhulla.com>
2025-10-10 17:36:15 +0530

    update on Fri Oct 10 05:36:15 PM IST 2025

---
+++ files/powermail-rootdir/usr/local/src/mailarchive-scripts/fetch-and-convert-and-add-to-index.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RECOLL_TMPDIR=/archivedata/index-temp-folder/
+
+
+if [ -f '/tmp/read-work-on-fetch-archive.pid' ];
+then
+   echo "File /tmp/read-work-on-fetch-archive.pid exists"
+else
+
+find /archivedata/mail-fetched/* -type d -empty -delete -print
+
+touch /tmp/read-work-on-fetch-archive.pid
+mkdir /archivedata/mail-fetched 2>/dev/null
+mkdir /archivedata/mail-archive-process/ 2>/dev/null
+mkdir /archivedata/index-temp-folder 2>/dev/null
+
+## from other location
+####rsync -av rbackup@192.192.10.22::mailarchive/* /archivedata/mail-fetched/ --remove-source-files --password-file=/usr/local/src/mailarchive-scripts/rpass
+
+
+find /archivedata/mail-archive-uncompress/ -type f -exec php read-mail-and-move-to-date-folder.php {} \;
+
+echo done.
+/bin/rm -rf /tmp/read-work-on-fetch-archive.pid
+fi
+
+
+
